df21a7fc99e424b629a054bce263d6abea8a1ee0
Ilmira Museeva <museevai@gmail.com>
2025-01-09 14:26:28 -0800

    Add Dependency Injection Logic

--- ServiceBusNotificationSender/ServiceBusNotificationSender/Program.cs
+++ ServiceBusNotificationSender/ServiceBusNotificationSender/Program.cs
@@ -1,7 +1,11 @@
+using Azure.Messaging.ServiceBus;
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ServiceBusNotificationSender;
+using ServiceBusNotificationSender.Dtos;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
@@ -17,5 +21,33 @@ builder.Configuration
     .AddJsonFile("local.settings.json", optional: true)
     .AddEnvironmentVariables()
     .AddUserSecrets<NotificationReceiver>();
+
+
+// Get the configuration object to read the Service Bus connection string and queue name.
+var config = builder.Configuration;
+
+// Fill the EmailSettingsDto to be used when we need to read settings in functions using dependency injection (i.e. passing IOptions<EmailSettingsDto> appSettings)
+builder.Services.AddOptions<EmailSettingsDto>()
+    .Configure<IConfiguration>((settings, configuration) =>
+    {
+        configuration.GetSection("EmailSettings").Bind(settings);
+    });
+
+// We need to create a Service Bus client to be used to send messages to the queue
+builder.Services.AddAzureClients(clientBuilder =>
+        {
+            clientBuilder.AddServiceBusClient(config.GetValue<string>("EmailSettings:NotificationBusConnection"));
+        });
+
+// It's safe to create a singleton sender
+// ServiceBusSender is the class that is used to send messages, it needs a ServiceBusClient
+// We used the previously created ServiceBusClient to instantiate the sender
+builder.Services.AddSingleton<ServiceBusSender>((provider) =>
+{
+    return provider
+        .GetRequiredService<ServiceBusClient>()
+        .CreateSender(config.GetValue<string>("EmailSettings:QueueName"));
+}
+);
 
 builder.Build().Run();
